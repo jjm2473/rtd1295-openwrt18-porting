857b1eb626a10fd4cefdb5a65085e03310728db1
jjm2473 <1129525450@qq.com>
2019-03-30 21:25:44 +0800

    auto mounting works.

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-mkdir root/overlay root/tmp root/proc root/sys root/dev root/mnt root/root root/www root/etc/crontabs root/etc/hotplug.d/dhcp root/etc/hotplug.d/neigh root/etc/hotplug.d/tftp root/usr/lib/iptables root/usr/lib/opkg/lists
-chmod 0755 root/overlay root/proc root/sys root/dev root/mnt root/root root/www root/etc/crontabs root/etc/hotplug.d/dhcp root/etc/hotplug.d/neigh root/etc/hotplug.d/tftp root/usr/lib/iptables root/usr/lib/opkg/lists
+# run this before build.sh
+mkdir root/android root/overlay root/tmp root/proc root/sys root/dev root/mnt root/root root/www root/etc/crontabs root/etc/hotplug.d/dhcp root/etc/hotplug.d/neigh root/etc/hotplug.d/tftp root/usr/lib/iptables root/usr/lib/opkg/lists
+chmod 0755 root/android root/overlay root/proc root/sys root/dev root/mnt root/root root/www root/etc/crontabs root/etc/hotplug.d/dhcp root/etc/hotplug.d/neigh root/etc/hotplug.d/tftp root/usr/lib/iptables root/usr/lib/opkg/lists
 chmod 1777 root/tmp
 sudo mknod root/dev/console c 0 0 && sudo chown `whoami` root/dev/console && chmod 0600 root/dev/console
--- overlay_pack.sh
+++ overlay_pack.sh
@@ -1,1 +1,2 @@
-find -type f -o -type l|grep -v dropbear_rsa_host_key|grep -v urandom.seed|xargs tar -czf /tmp/update.tar.gz
+# run on device /overlay/upper/
+find -type f -o -type l|grep -v dropbear_rsa_host_key|grep -v urandom.seed|grep -v board.json|xargs tar -czf /tmp/update.tar.gz
