b16013be727495ca36f02d6e6684e0c64e027b6e
jjm2473 <1129525450@qq.com>
2019-03-30 23:57:07 +0800

    base luci.

--- overlay_pack.sh
+++ overlay_pack.sh
@@ -1,2 +1,3 @@
-# run on device /overlay/upper/
-find -type f -o -type l|grep -v dropbear_rsa_host_key|grep -v urandom.seed|grep -v board.json|xargs tar -czf /tmp/update.tar.gz
+# use opkg offline mode `opkg -o / -f /etc/opkg.conf install ??` to install, then
+# run this on device /overlay/upper/ to package
+find -type f -o -type l|grep -v ./mnt|grep -v dropbear_rsa_host_key|grep -v urandom.seed|grep -v board.json|xargs tar -czf /tmp/update.tar.gz
