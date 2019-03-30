64a6359da8ae9f2b27b649808725a627b3535646
jjm2473 <1129525450@qq.com>
2019-03-31 01:55:26 +0800

    add samba, minidlna, transmission.

--- overlay_pack.sh
+++ overlay_pack.sh
@@ -1,3 +1,4 @@
 # use opkg offline mode `opkg -o / -f /etc/opkg.conf install ??` to install, then
 # run this on device /overlay/upper/ to package
+find etc/init.d/ -type f|xargs -n1 sh -c '/$0 enable'
 find -type f -o -type l|grep -v ./mnt|grep -v dropbear_rsa_host_key|grep -v urandom.seed|grep -v board.json|xargs tar -czf /tmp/update.tar.gz
