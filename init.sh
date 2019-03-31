21fb9cf371a78610c877a1a56911c2daefb0e5f2
jjm2473 <1129525450@qq.com>
2019-03-31 23:50:43 +0800

    fix init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,18 @@ mkdir root/android root/overlay root/tmp root/proc root/sys root/dev root/mnt ro
 chmod 0755 root/android root/overlay root/proc root/sys root/dev root/mnt root/root root/www root/etc/crontabs root/etc/hotplug.d/dhcp root/etc/hotplug.d/neigh root/etc/hotplug.d/tftp root/usr/lib/iptables root/usr/lib/opkg/lists
 chmod 1777 root/tmp
 sudo mknod root/dev/console c 0 0 && sudo chown `whoami` root/dev/console && chmod 0600 root/dev/console
+echo './proc
+./cache
+./config
+./sbin
+./oem
+./storage
+./system
+./sys
+./mnt
+./dev
+./acct
+./data' | xargs -n1 sh -c 'mkdir -p root/mnt/android/$0'
+
+chmod 750 root/mnt/android/data
+chmod 771 root/mnt/android/sbin
