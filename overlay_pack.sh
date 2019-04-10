d510726941ac3db505e16007c91994345cec7f10
jjm2473 <1129525450@qq.com>
2019-04-10 23:36:22 +0800

    use https opkg feed.

--- overlay_pack.sh
+++ overlay_pack.sh
@@ -1,4 +1,4 @@
 # use opkg offline mode `opkg -o / -f /etc/opkg.conf install ??` to install, then
 # run this on device /overlay/upper/ to package
 find etc/init.d/ -type f|xargs -n1 sh -c '/$0 enable'
-find -type f -o -type l|grep -v ./mnt|grep -v dropbear_rsa_host_key|grep -v urandom.seed|grep -v board.json|xargs tar -czf /tmp/update.tar.gz
+find -type f -o -type l|grep -v ./mnt|grep -v ./root/|grep -v dropbear_rsa_host_key|grep -v urandom.seed|grep -v board.json|grep -v ./etc/config/|grep -v ./etc/samba/|xargs tar -czf /tmp/update.tar.gz
