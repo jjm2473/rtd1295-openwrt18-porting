6904b04cc8ecf5f8824a6d183fa9f3797a2b17ec
jjm2473 <1129525450@qq.com>
2019-04-13 13:32:00 +0800

    fix tar file list

--- overlay_pack.sh
+++ overlay_pack.sh
@@ -1,2 +1,2 @@
 # run on device /overlay/upper/
-find -type f -o -type l|grep -v dropbear_rsa_host_key|grep -v urandom.seed|grep -v board.json|xargs tar -czf /tmp/update.tar.gz
+find -type f -o -type l|grep -v dropbear_rsa_host_key|grep -v urandom.seed|grep -v board.json|tar -czf /tmp/update.tar.gz -T -
