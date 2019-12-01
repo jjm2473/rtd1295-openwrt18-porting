569c3abcbfe8f5f5791d2431fb1486b590ff3453
jjm2473 <1129525450@qq.com>
2019-12-01 10:31:08 +0800

    update from https://downloads.openwrt.org/releases/18.06.5/targets/brcm2708/bcm2710/openwrt-18.06.5-brcm2708-bcm2710-rpi-3-squashfs-sysupgrade.img.gz sha256sum: 6bdedfc57e307e04a08e53b7fc74a77c3071e831b3189e0777e21402a650a89e

--- root/lib/upgrade/platform.sh
+++ root/lib/upgrade/platform.sh
@@ -1,5 +1,13 @@
+. /lib/functions.sh
+
+REQUIRE_IMAGE_METADATA=1
+
+# copied from x86's platform.sh
+
 platform_check_image() {
 	local diskdev partdev diff
+
+	[ "$#" -gt 1 ] && return 1
 
 	export_bootdevice && export_partdevice diskdev -2 || {
 		echo "Unable to determine upgrade device"
@@ -23,16 +31,8 @@ platform_check_image() {
 		ask_bool 0 "Abort" && exit 1
 		return 0
 	fi
-}
 
-platform_copy_config() {
-	local partdev
-
-	if export_partdevice partdev -1; then
-		mount -t vfat -o rw,noatime "/dev/$partdev" /mnt
-		cp -af "$CONF_TAR" /mnt/
-		umount /mnt
-	fi
+	return 0;
 }
 
 platform_do_upgrade() {
@@ -60,7 +60,7 @@ platform_do_upgrade() {
 	fi
 
 	if [ -n "$diff" ]; then
-		get_image "$@" | dd of="/dev/$diskdev" bs=4096 conv=fsync
+		get_image "$@" | dd of="/dev/$diskdev" bs=2M conv=fsync
 
 		# Separate removal and addtion is necessary; otherwise, partition 1
 		# will be missing if it overlaps with the old partition 2
@@ -70,20 +70,35 @@ platform_do_upgrade() {
 		return 0
 	fi
 
-	#write uboot image
-	get_image "$@" | dd of="$diskdev" bs=1024 skip=8 seek=8 count=1016 conv=fsync
 	#iterate over each partition from the image and write it to the boot disk
 	while read part start size; do
-		part="$(($part - 2))"
+		# root is /dev/sd[a|b]2 and not /dev/sd[a|b] this causes some problem
+		# one of which is this offset, I'm not sure what's the best fix, so
+		# here's a WA.
+		let part=$((part - 2))
 		if export_partdevice partdev $part; then
 			echo "Writing image to /dev/$partdev..."
 			get_image "$@" | dd of="/dev/$partdev" ibs="512" obs=1M skip="$start" count="$size" conv=fsync
 		else
 			echo "Unable to find partition $part device, skipped."
-		fi
+	fi
 	done < /tmp/partmap.image
 
 	#copy partition uuid
 	echo "Writing new UUID to /dev/$diskdev..."
 	get_image "$@" | dd of="/dev/$diskdev" bs=1 skip=440 count=4 seek=440 conv=fsync
+}
+
+platform_copy_config() {
+	local partdev
+
+	# Same as above /dev/sd[a|b]2 is root, so /boot is -1
+	if export_partdevice partdev -1; then
+		mkdir -p /boot
+		[ -f /boot/kernel.img ] || mount -t vfat -o rw,noatime "/dev/$partdev" /boot
+		cp -af "$CONF_TAR" /boot/
+		tar --directory / -xvf "$CONF_TAR" boot/config.txt
+		sync
+		unmount /boot
+	fi
 }
